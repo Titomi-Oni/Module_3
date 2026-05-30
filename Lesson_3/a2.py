58186ac5dbadf9edce09cd0b0b2e7b461d4d3a39
Titomi <onititomi@gmail.com>
2026-05-29 20:06:14 -0600

    Module_3 Test

--- Lesson_3/a2.py
+++ Lesson_3/a2.py
@@ -2,17 +2,27 @@
 import random
 # 2) Start an infinite loop using `while True` so the game can repeat for multiple rounds.
 while True:
-    user_action = input("Pick your choice: rock, paper or scissors:  ")
+   user_action = input("Pick your choice: Rock, Paper or scissors:  ")
 # 3) Take the user's choice as input and store it in `user_action`.
 #    (Expected inputs: "rock", "paper", or "scissors".)
 # 4) Create a list `possible_actions` containing the three valid moves.
-    possible_actions = ["rock", "paper", "scissors"]
+possible_actions = ["rock", "paper", "scissors"]
 # 5) Use `random.choice(possible_actions)` to randomly select the computer’s move
 #    and store it in `computer_action`.
     computer_action = random.choice (possible_actions)
 # 6) Display both choices (user and computer) using an f-string.
-    print (f"\n You chose: {user_action}, Computer chose: {computer_action}")
-
+    print (f"\n You chose: {user_action}, computer chose: {computer_action}")
+# 7) Compare `user_action` and `computer_action` to decide the result:
+#    a) If both are the same, print that it’s a tie.
+#    b) Else if the user chose "rock":
+#       i) If computer chose "scissors", user wins.
+#       ii) Otherwise, user loses (computer chose "paper").
+#    c) Else if the user chose "paper":
+#       i) If computer chose "rock", user wins.
+#       ii) Otherwise, user loses (computer chose "scissors").
+#    d) Else if the user chose "scissors":
+#       i) If computer chose "paper", user wins.
+#       ii) Otherwise, user loses (computer chose "rock").
     if user_action == computer_action:
          print ("Tie")
     elif user_action == 'rock':
---
+++ Test/test.py
@@ -0,0 +1,26 @@
+operation = input("Please pick an operation \n 1) Addition \n 2) Subtraction \n 3) Multiplication \n 4) Division: " )
+
+num1 = float(input("Please enter your first number for: "))
+num2 = float(input("Please enter your second number for: "))
+
+
+def add(num1, num2):
+    print (num1+num2)
+    
+    return (num1+num2)
+
+
+def subtract(num1,num2):
+    print (num1-num2)
+    
+    return (num1-num2)
+
+def multiply(num1,num2):
+    print (num1*num2)
+
+    return (num1*num2)
+
+def divide(num1,num2):
+    print (num1/num2)
+
+    return (num1/num2)
